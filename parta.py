7e2b13157525240ceeee456aaa5c2aa32dcdf9c8
valerieehuynh <vlhuynh2@uci.edu>
2025-01-23 20:43:14 -0800

    finish common word function and add main

--- parta.py
+++ parta.py
@@ -38,5 +38,50 @@ def tokenize(file):
     
     return result
 
+def compute_word_frequencies(tokens):
+    result = {}
+
+    for i in tokens:
+
+        if i not in result:
+            result[i] = 1
+            
+        else:
+            result[i] = result[i] + 1
+            
+    return result
+
+def common_words(file1_path, file2_path):
+    t1 = set(tokenize(file1_path))
+    t2 = set(tokenize(file2_path))
+
+
+    common_words = t1.intersection(t2)
+
+    return len(common_words)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        print("Error. Use it like this: python PartB.py TextFilePath1 TextFilePath2")
+        sys.exit(1)
+
+    f1 = sys.argv[1]
+    f2 = sys.argv[2]
+
+    try:
+        
+        result = common_words(f1, f2)
+
+        
+        print(result)
+
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+    except FileNotFoundError as e:
+        print(f"File not Found: {e}")
+        sys.exit(1)
 
 
